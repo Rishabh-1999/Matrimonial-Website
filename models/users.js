7a34a52eaa911ce93a1376baba1b1c8faa5ecfd2
Rishabh-1999 <rishabhanand33@gmail.com>
2020-01-30 20:02:09 +0530

    Schema Added

---
+++ models/users.js
@@ -0,0 +1,90 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var loginSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    middlename: {
+        type: String,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    religion: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    DOB: {
+        type: String,
+    },
+    mothertongue: {
+        type: String,
+        trim: true
+    },
+    phoneno: {
+        type: String,
+        trim: true
+    },
+    photourl: {
+        type: String,
+        trim: true
+    },
+    education: {
+        type: String,
+        trim: true
+    },
+    height: {
+        type: String,
+        trim: true
+    },
+    weight: {
+        type: String,
+        trim: true
+    },
+    address1: {
+        type: String,
+        trim: true
+    },
+    city: {
+        type: String,
+        trim: true
+    },
+    state: {
+        type: String,
+        trim: true
+    },
+    isVerifed: {
+        type: Boolean,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        required: true
+    },
+});
+
+var logins = mongoose.model("users", loginSchema);
+module.exports = logins;
